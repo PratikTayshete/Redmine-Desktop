27a2a5cd6fae1b5843d3d6979ec4b514aa03269b
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-12-02 03:52:45 +0530

    Added validation for login process in the application.

--- libs/redmine.py
+++ libs/redmine.py
@@ -4,16 +4,19 @@ from redminelib import Redmine
 class RedMineManager:
     def __init__(self, username, password):
         self.username = username
-        self.__valid_redmine_login = False
-        self.__validate_login(self.username, password)
+        self.password = password
+        self.__valid_redmine_login = None
+        self.redmine = Redmine("http://127.0.0.1/redmine", username=self.username, password=self.password)
 
-    def __validate_login(self, username, password):
+    def validate_login(self):
+        is_user_valid = None
         try:
-            self.redmine = Redmine("http://127.0.0.1/redmine", username=username, password=password)
             if self.redmine.auth():
                 self.__valid_redmine_login = True
+                is_user_valid = True
         except Exception as error:
             print(error)
+        return is_user_valid
 
     def get_projects(self):
         if self.__valid_redmine_login:
