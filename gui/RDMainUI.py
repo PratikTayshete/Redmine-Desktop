de119010b6857bfec87d884ca563ba2673947cdf
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-12-02 03:41:21 +0530

    Added a user interface for the main window of the desktop application.

---
+++ gui/RDMainUI.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'RDMainUI.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_RDMain(object):
+    def setupUi(self, RDMain):
+        if not RDMain.objectName():
+            RDMain.setObjectName(u"RDMain")
+        RDMain.resize(800, 600)
+        self.centralwidget = QWidget(RDMain)
+        self.centralwidget.setObjectName(u"centralwidget")
+        RDMain.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(RDMain)
+
+        QMetaObject.connectSlotsByName(RDMain)
+    # setupUi
+
+    def retranslateUi(self, RDMain):
+        RDMain.setWindowTitle(QCoreApplication.translate("RDMain", u"MainWindow", None))
+    # retranslateUi
+
