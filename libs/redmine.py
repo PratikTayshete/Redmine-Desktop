b3585d2f886ecb25789434ee38393ebbf01bc967
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-12-07 04:02:16 +0530

    Displays multiple attributes related to issues created by the user. Also added the functionality for logout and refresh button.  - Displays the following attributes related to issues:     - Total issues count.     - Total projects.     - Total issues resolved.     - Total issues pending.

--- libs/redmine.py
+++ libs/redmine.py
@@ -1,4 +1,5 @@
 from redminelib import Redmine
+from utils import constants
 
 
 class RedMineManager:
@@ -6,9 +7,14 @@ class RedMineManager:
         self.username = username
         self.password = password
         self.__valid_redmine_login = None
+        self.projects_list = None
+        self.issues_dict = None
         self.redmine = Redmine("http://127.0.0.1/redmine", username=self.username, password=self.password)
 
     def validate_login(self):
+        """
+        Validates the login credentials of the user.
+        """
         is_user_valid = None
         try:
             if self.redmine.auth():
@@ -19,6 +25,80 @@ class RedMineManager:
         return is_user_valid
 
     def get_projects(self):
+        """
+        Gets the list of projects.
+        :return: projects_list: A list of names of all the projects.
+        """
         if self.__valid_redmine_login:
-            projects_list = [project.name for project in self.redmine.project.all()]
+            self.projects_list = [project.name for project in self.redmine.project.all()]
+            projects_list = self.projects_list
             return projects_list
+
+    def total_projects_count(self):
+        """
+        Gets the total number of projects.
+        :return: total_projects: A total count of all the projects.
+        """
+        total_projects = str(len(self.get_projects()))
+        return total_projects
+
+    def get_issues(self):
+        """
+        Gets all the issues created by the user.
+        :return: issues_dict: A dictionary that contains issues created by the user along with some of its information.
+        """
+        if self.__valid_redmine_login:
+            self.issues_dict = {}
+            for issue in self.redmine.issue.all():
+                issue_dict = {'subject': issue.subject,
+                              'status': str(issue.status),
+                              'priority': str(issue.priority),
+                              'description': issue.description
+                              }
+                self.issues_dict[issue.id] = issue_dict
+        issues_dict = self.issues_dict
+        return issues_dict
+
+    def total_issues_count(self):
+        """
+        Gets the total number of issues created by the user.
+        :return: total_issues: A total count of all the issues.
+        """
+        if self.__valid_redmine_login:
+            total_issues = str(len(self.get_issues()))
+            return total_issues
+
+    def total_issues_resolved_count(self):
+        """
+        Gets the total number of issues whose status is resolved.
+        :return: total_resolved: A total count of issues whose status is resolved.
+        """
+        if self.__valid_redmine_login:
+            total_resolved = 0
+            for issue_key in self.issues_dict:
+                if self.issues_dict[issue_key]['status'] == constants.RESOLVED_STATUS:
+                    total_resolved += 1
+            total_resolved = str(total_resolved)
+            return total_resolved
+
+    def total_issues_pending_count(self):
+        """
+        Gets the total number of issues whose status is not resolved.
+        :return: total_pending: A total count of issues whose status is not resolved.
+        """
+        if self.__valid_redmine_login:
+            total_pending = 0
+            for issue_key in self.issues_dict:
+                if self.issues_dict[issue_key]['status'] != constants.RESOLVED_STATUS:
+                    total_pending += 1
+            total_pending = str(total_pending)
+            return total_pending
+
+    def clear_data_on_logout(self):
+        """
+        Reassigns variables to None.
+        """
+        self.projects_list = None
+        self.issues_dict = None
+        self.redmine = None
+        self.__valid_redmine_login = None
--- redmine_desktop.py
+++ redmine_desktop.py
@@ -1,35 +1,90 @@
 from PySide2.QtWidgets import QMainWindow, QApplication
-from gui.RDLoginUI import Ui_RDLogin
+from gui.RDAppUI import Ui_RDApp
 from libs.redmine import RedMineManager
-from modules.ui import RedmineDesktopMain
+from utils import operations
 
 
-class RedmineDesktop(QMainWindow, Ui_RDLogin):
+class RedmineDesktop(QMainWindow, Ui_RDApp):
     def __init__(self):
         QMainWindow.__init__(self)
-        Ui_RDLogin.__init__(self)
+        Ui_RDApp.__init__(self)
         self.setupUi(self)
         self.redmine_manager = None
         self.is_user_valid = None
-        self.main_rd_app = None
+        self.rdStackWidget.setCurrentIndex(0)
         self.connect_interface()
 
     def connect_interface(self):
+        """
+        Connects actions performed on widgets to its functionalities.
+        """
         self.loginButton.clicked.connect(self.login)
+        self.logoutButton.clicked.connect(self.logout)
+        self.refreshButton.clicked.connect(self.load_data)
 
     def login(self):
+        """
+        Validates the user and logs in.
+        """
         username = str(self.usernameLoginLinedit.text())
         password = str(self.passwordLoginLinedit.text())
         self.redmine_manager = RedMineManager(username=username, password=password)
         self.is_user_valid = self.redmine_manager.validate_login()
         if self.is_user_valid:
+            self.usernameLoginLinedit.clear()
+            self.passwordLoginLinedit.clear()
             print("[INFO]User [{}] successfully logged in.".format(username))
-            self.close()
-            self.main_rd_app = RedmineDesktopMain(redmine_manager=self.redmine_manager)
-            self.main_rd_app.launch()
-
+            self.rdStackWidget.setCurrentIndex(1)
+            self.load_data()
         else:
             print("[INFO] Invalid login.")
+            operations.display_message("Invalid Login", "Please check the login credentials.")
+
+    def load_data(self):
+        """
+        Loads the data in the application.
+        """
+        self.load_total_projects()
+        self.load_total_issues()
+        self.load_total_issues_resolved()
+        self.load_total_pending_issues()
+
+    def load_total_pending_issues(self):
+        """
+        Display total pending issues.
+        """
+        total_issues_pending = self.redmine_manager.total_issues_pending_count()
+        self.totalPendingCountLabel.setText(total_issues_pending)
+
+    def load_total_issues_resolved(self):
+        """
+        Display total issues resolved.
+        """
+        total_issues_resolved = self.redmine_manager.total_issues_resolved_count()
+        self.totalResolvedCountLabel.setText(total_issues_resolved)
+
+    def load_total_issues(self):
+        """
+        Display total issues.
+        """
+        total_issues = self.redmine_manager.total_issues_count()
+        self.totalIssuesCountLabel.setText(total_issues)
+
+    def load_total_projects(self):
+        """
+        Display total projects.
+        """
+        total_projects = self.redmine_manager.total_projects_count()
+        self.totalProjectsCountLabel.setText(total_projects)
+
+    def logout(self):
+        """
+        Logs out the user from the application.
+        """
+        self.redmine_manager.clear_data_on_logout()
+        self.redmine_manager = None
+        self.is_user_valid = None
+        self.rdStackWidget.setCurrentIndex(0)
 
 
 if __name__ == '__main__':
