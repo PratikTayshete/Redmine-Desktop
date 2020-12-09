5c1dfde0932f15b2a252247d0d5f8f8b5360ff02
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-12-10 03:37:47 +0530

    Added empty QDialog to insert new issue.

---
+++ gui/IssueDialogUI.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'IssueDialogUI.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_CreateIssue(object):
+    def setupUi(self, CreateIssue):
+        if not CreateIssue.objectName():
+            CreateIssue.setObjectName(u"CreateIssue")
+        CreateIssue.resize(622, 449)
+        CreateIssue.setStyleSheet(u"QDialog {\n"
+"	background-color: rgb(66, 66, 66);\n"
+"	border-radius: 25px;\n"
+"}")
+
+        self.retranslateUi(CreateIssue)
+
+        QMetaObject.connectSlotsByName(CreateIssue)
+    # setupUi
+
+    def retranslateUi(self, CreateIssue):
+        CreateIssue.setWindowTitle(QCoreApplication.translate("CreateIssue", u"Create Issue", None))
+    # retranslateUi
+
