b3524a538dacbf064b0ac945106aed83cfbc613f
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-12-14 04:08:15 +0530

    Constant values used in the application.

--- utils/constants.py
+++ utils/constants.py
@@ -2,3 +2,4 @@
 This file holds constant variables used throughout the application.
 """
 RESOLVED_STATUS = 'Resolved'
+PRIORITIES_DICT = {"Low": 1, "Normal": 2, "High": 3, "Urgent": 4, "Immediate": 5}
